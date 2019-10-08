b4dc50d6f51e6eadd4ac95325467e6552b183e55
Brazen <764146802@qq.com>
2019-10-08 23:48:09 +0800

    准备写阅读计时功能

--- src/views/ebook/index.vue
+++ src/views/ebook/index.vue
@@ -10,9 +10,10 @@
   import EbookReader from '../../components/ebook/EbookReader.vue'
   import EbookTitle from '../../components/ebook/EbookTitle.vue'
   import EbookMenu from '../../components/ebook/EbookMenu.vue'
-
+  import { ebookMixin } from '../../utils/mixin'
 
   export default {
+    mixins: [ebookMixin],
     components: {
       EbookReader,
       EbookTitle,
