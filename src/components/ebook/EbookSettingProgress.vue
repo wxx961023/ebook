0ff623ceda9b04dab3ec7f00a033397876ff7408
Brazen <764146802@qq.com>
2019-10-10 21:43:46 +0800

    完成阅读时间

--- src/components/ebook/EbookSettingProgress.vue
+++ src/components/ebook/EbookSettingProgress.vue
@@ -3,7 +3,7 @@
     <div class="setting-wrapper" v-show="menuVisible && settingVisible === 2">
       <div class="setting-progress">
         <div class="read-time-wrapper">
-          <span class="read-time-text">{{'111'}}</span>
+          <span class="read-time-text">{{getReadTimeText()}}</span>
           <span class="icon-forward"></span>
         </div>
         <div class="progress-wrapper">
@@ -37,6 +37,7 @@
 
 <script>
 import { ebookMixin } from '../../utils/mixin.js'
+import { getReadTime } from '../../utils/localStorage'
 export default {
   mixins: [ ebookMixin ],
   data() {
@@ -93,7 +94,17 @@ export default {
           this.display(sectionInfo.href)
         }
     },
-
+    getReadTimeText() {
+      return this.$t('book.haveRead').replace('$1', this.getReadTimeByMinute())
+    },
+    getReadTimeByMinute() {
+      let readTime = getReadTime(this.fileName)
+      if(!readTime){
+        return 0
+      }else{
+        return Math.ceil(readTime / 60)
+      }
+    }
   },
   updated() {
     this.updateProgressBg()
--- src/views/ebook/index.vue
+++ src/views/ebook/index.vue
@@ -11,6 +11,7 @@
   import EbookTitle from '../../components/ebook/EbookTitle.vue'
   import EbookMenu from '../../components/ebook/EbookMenu.vue'
   import { ebookMixin } from '../../utils/mixin'
+  import { getReadTime, saveReadTime } from '../../utils/localStorage'
 
   export default {
     mixins: [ebookMixin],
@@ -18,7 +19,29 @@
       EbookReader,
       EbookTitle,
       EbookMenu
-    }
+    },
+    methods: {
+      startLoopReadTime() {
+        let readTime = getReadTime(this.fileName)
+        if(!readTime) {
+          readTime = 0
+        }
+        this.task = setInterval(() => {
+          readTime++
+          if(readTime % 30 === 0) {
+            saveReadTime(this.fileName, readTime)
+          }
+        },1000)
+      }
+    },
+    mounted () {
+      this.startLoopReadTime()
+    },
+    beforeDestroy () {
+      if(this.task) {
+        clearInterval(this.task)
+      }
+    },
   }
 </script>
 
